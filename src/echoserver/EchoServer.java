4f40eb362654986daa44f5313cd355ce30a08d3e
dragoteon <woest015@morris.umn.edu>
2017-12-12 12:28:51 -0600

    Did the server side of the code with comments.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,28 +1,54 @@
 package echoserver;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
-public class EchoServer {
+public class EchoServer implements Runnable {
 	public static final int PORT_NUMBER = 6013;
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		EchoServer server = new EchoServer();
-		server.start();
+		try{
+			//set up socket server
+			ServerSocket iSocketXetServe = new ServerSocket(6013);
+			
+			ExecutorService threaded = Executors.newFixedThreadPool(2);
+			
+			//executing new thread for sockets
+		while(true){
+			Socket newSocket2_0 = iSocketXetServe.accept();
+			badName ThreadServer = new badName(newSocket2_0);
+			threaded.execute(ThreadServer);
+		}
+		//expeception handling with conveniet failure tag
+	}catch (IOException FAILURE){
+		System.err.println(FAILURE);
+	}
 	}
 
-	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
-			InputStream inputStream = socket.getInputStream();
-			OutputStream outputStream = socket.getOutputStream();
-			int b;
-			while ((b = inputStream.read()) != -1) {
-				outputStream.write(b);
+
+	//terrible naming scheme 
+	public static class badName extends Thread {
+		Socket iSocketX;
+		public badName(Socket iSocketX) {
+			this.iSocketX = iSocketX;
+		}
+
+		//Setting up input and output stream.
+		public void run() {
+			try{
+				OutputStream out = iSocketX.getOutputStream();
+				InputStream in = iSocketX.getInputStream();
+				
+				int Done;
+				while((Done = in.read()) != -1) {
+					out.write(Done);
+				}
+			
+				out.flush();
+				iSocketX.shutdownOutput();
+				iSocketX.close();
+			} catch (IOException CONTINUOUS_FAILURE) {
+				System.err.println(CONTINUOUS_FAILURE);
 			}
 		}
 	}
